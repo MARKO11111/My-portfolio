be88dc4c4ee4a2946e1c7a44d96108368a5162bc
Marko Bilic <bilmarko@gmail.com>
2023-08-02 18:48:05 +0200

    Added scroll behaviour

---
+++ js/content-scroll.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll('section');
+const links = document.querySelectorAll('.nav_link');
+
+window.addEventListener('scroll', () => {
+    let scrollPosition = window.scrollY + 480;
+
+    sections.forEach(section => {
+        if (scrollPosition >= section.offsetTop) {
+            links.forEach(link => {
+                link.classList.remove('active');
+                if (section.getAttribute('id') === link.getAttribute('href').substring(1)) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+});
+
+console.log(sections);
